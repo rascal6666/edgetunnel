d28334001f31af264b20ae1d3b2a3408b2bc2d15
rascal6666 <119034145+rascal6666@users.noreply.github.com>
2025-04-27 10:43:05 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -23,7 +23,7 @@ let go2Socks5s = [
 	'*.loadshare.org',
 ];
 let addresses = [];
-let addressesapi = [];
+let addressesapi = ["https://raw.githubusercontent.com/rascal6666/cfipcaiji/refs/heads/main/ip.txt"];
 let addressesnotls = [];
 let addressesnotlsapi = [];
 let addressescsv = [];
